dedfcb6f85230d2fbf111f1e2bcc1f0dd21b81e3
zhuan69 <zhuan.akb@gmail.com>
2021-01-12 13:38:51 +0700

    New update participant list

--- src/app/tournament/tournament.module.ts
+++ src/app/tournament/tournament.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TournamentRoutingModule } from './tournament-routing.module';
 import { TournamentComponent } from './tournament.component';
+import { InfoBadmintonComponent } from './info-tournament/info-tournament..component';
+import { ListTournamentComponent } from './list-team/list-team.component';
 
 @NgModule({
-  declarations: [TournamentComponent],
+  declarations: [
+    TournamentComponent,
+    InfoBadmintonComponent,
+    ListTournamentComponent,
+  ],
   imports: [CommonModule, TournamentRoutingModule],
 })
 export class TournamentModule {}
